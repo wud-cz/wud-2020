b3a15f1b9e2b9a2e49ce655489ed0f8ce00a1b54
Filip Jirsák <jirsakf@oksystem.cz>
2020-08-10 18:35:51 +0200

    Zvýrazňování formulářových prvků.

---
+++ src/main/script/menu.js
@@ -0,0 +1,24 @@
+document.getElementById('menu-cfp').addEventListener('click', function (event) {
+    window.setTimeout(function() {
+        document.getElementById('cfp-email').focus();
+    }, 100)
+})
+
+document.getElementById('menu-contact').addEventListener('click', function (event) {
+    window.setTimeout(function() {
+        document.getElementById('contact').classList.add('glow');
+    }, 300)
+    window.setTimeout(function() {
+        document.getElementById('contact').classList.remove('glow');
+    }, 1000)
+})
+
+document.getElementById('menu-contact-me').addEventListener('click', function (event) {
+    window.setTimeout(function() {
+        document.getElementById('contact-me-email').focus();
+        document.getElementById('contact-form').classList.add('glow');
+    }, 100)
+    window.setTimeout(function() {
+        document.getElementById('contact-form').classList.remove('glow');
+    }, 1000)
+})
